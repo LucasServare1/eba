cf088b51fe2fef298d668f4715e88d7bc2d301a0
Lucão 22 cm <142432774+LucasServare1@users.noreply.github.com>
2024-09-30 11:08:53 -0300

    dsadasd

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-let nomeUsuario = 'Lucas I, Gabriel L';
-// alert (nomeUsuario);
-function soma (a,b){
-    return a+b;
-    console.log(soma(1,2));
+// let nomeUsuario = 'Lucas I, Gabriel L';
+// // alert (nomeUsuario);
+// function soma (a,b){
+//     return a+b;
+//     console.log(soma(1,2));
     
-}
-function multiplicar (a,b){
-    return a*b;
-console.log(multiplicar(6,6));
-}
-function dividir (a,b){
-    return a/b;
-console.log(dividir(4,2));
-}
-let areaTriangulo = function(a,b){
-    return a*b/2;
+// }
+// function multiplicar (a,b){
+//     return a*b;
+// console.log(multiplicar(6,6));
+// }
+// function dividir (a,b){
+//     return a/b;
+// console.log(dividir(4,2));
+// }
+// let areaTriangulo = function(a,b){
+//     return a*b/2;
 
-}
-console.log(areaTriangulo);
-let nome= parseInt (prompt('?'));
-document.write(nome);
+// }
+// console.log(areaTriangulo);
+// let nome= parseInt (prompt('?'));
+// document.write(nome);
 
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
